d673ae3162e6eca0e1d129e8e56890fc26068013
dan-seol <yunheum.seol@mail.mcgill.ca>
2024-02-19 08:03:32 +0000

    refactoring and adding isometry

--- smooth_hamiltonian.py
+++ smooth_hamiltonian.py
@@ -15,6 +15,36 @@ def get_ket(j, N):
     ket[j] = 1
     return ket
 
+
+def get_hyperplane_term(j, A, N):
+    for k in np.nonzero(A[j]):
+        first_term = np.sqrt(np.conj(A[j][k]))*get_ket(k, N)
+        second_term = np.sqrt(1-np.abs(A[j][k]))*get_ket(k+N, N)
+        yield first_term + second_term
+
+
+def get_hyperplane_basis(A, N, d):
+    """
+    Define hyperplane basis to get Chebyshev polynomials on a quantum circuit.
+    We assume the matrix A is d-sparse https://faculty.ucmerced.edu/mhyang/course/eecs275/lectures/lecture26.pdf
+    """
+    for j in range(N):   # (33)
+        yield np.outer(
+            np.kron(get_ket(j, N), 1/np.sqrt(d)*sum(get_hyperplane_term(j, A, N))),
+            get_ket(j, N)
+        )
+
+
+def get_isometry(A, N, d):
+    """Define isometry T from the hyperplane basis."""
+    return np.sum(list(get_hyperplane_basis(A, N, d)), 0)
+
+
+def walk(A, N):
+    d = np.linalg.matrix_rank(A, hermitian=True)
+    T = get_isometry(A, N, d)
+    return 2*(T @ np.conj(T)) - np.eye(2N)
+
 # Create a QNode
 @qml.qnode(dev)
 def circuit(A, f):
@@ -36,7 +66,8 @@ def circuit(A, f):
             if A[j][k] != 0.:
                  sum_aij_nonzero += first_term + second_term
         get_ket(j, N) @ 1/np.sqrt(d)*sum_aij_nonzero   # (33)
-    # define isometry T from phi some multi swap operator(3 rd ingredient),
+    # define isometry T from phi
+    # some multi swap operator(3 rd ingredient),
     #
     # can be replaced with multiple swap gates S define W from S and T  (36)
 
